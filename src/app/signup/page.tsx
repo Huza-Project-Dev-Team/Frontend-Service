5a862ed103c864508761904678722769500c489a
utujesandrine456 <utujesandrine456@gmail.com>
2025-09-10 08:26:16 +0200

    signup changed

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -128,7 +128,7 @@ export default  function Signup(){
                                         </div>
                                         )}
                                     </div>
-                                    </div>
+                                </div>
                             </div>
                             
 
